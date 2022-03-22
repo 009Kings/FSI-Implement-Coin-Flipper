253c6f0e98fc9f52f1e92ffccaa0831321a3d22e
King <009kings@gmail.com>
2022-03-21 18:21:22 -0700

    functional flip game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,51 @@
 // TODO: Declare any global variables we need
-
+let count = {
+    heads: 0,
+    tails: 0
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+    document.querySelector('#flip').addEventListener('click', function(e) {
 
-    // TODO: Add event listener and handler for flip and clear buttons
+        function handleFlip(result) {
+            // result = 'heads' || 'tails'
+            console.log(result)
+            count[result]++ // count.heads || count['heads']
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
+            // DOM STUFF
+            let img = document.querySelector('img')
+            img.src = `assets/images/penny-${result}.jpg`
+            img.alt = `${result} face of a penny`
 
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
+            let message = document.querySelector('.message-container h3')
+            message.textContent = `You flipped ${result}!`
 
+            // Update scorecard
+            let total = count.heads + count.tails
+            // Update Numbers
+            document.querySelector(`#${result}`).textContent = count[result]
+            document.querySelector('#heads-percent').textContent = Math.round(count.heads/total * 100) + "%"
+            document.querySelector('#tails-percent').textContent = Math.round(count.tails/total * 100) + "%"
+        }
 
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+        // Determine flip outcome
+        if (Math.random() < .5) {
+            handleFlip('heads')
+        } else {
+            handleFlip('tails')
+        }
+    })
 
+    document.querySelector('#clear').addEventListener('click', function(e) {
+        console.log('cleared the scoreboard')
+        count.heads = 0
+        count.tails = 0
+
+        document.querySelector('#heads').textContent = 0
+        document.querySelector('#heads-percent').textContent = '0%'
+        document.querySelector('#tails').textContent = 0
+        document.querySelector('#tails-percent').textContent = '0%'
+
+        document.querySelector('.message-container h3').textContent = "Let's Get Rolling"
+    })
 })
